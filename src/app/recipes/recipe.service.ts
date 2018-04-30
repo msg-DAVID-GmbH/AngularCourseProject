3a81ef6a5f915dad61a592c21148a2507609e703
Steven Schwenke <steven@stevenschwenke.de>
2018-04-30 14:01:10 +0200

    Add Ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,19 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes = [
-      new Recipe('Recipe 1', 'This is a test',
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkbElx8kYynTvJxzV0HkoXgPHqAJ96aal_vHRuLT4tanPbUqnq'),
-      new Recipe('Recipe 2', 'This is a test',
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkbElx8kYynTvJxzV0HkoXgPHqAJ96aal_vHRuLT4tanPbUqnq')
-    ];
+    new Recipe('Recipe 1', 'This is a test',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkbElx8kYynTvJxzV0HkoXgPHqAJ96aal_vHRuLT4tanPbUqnq'
+      , [new Ingredient('Carrot', 1), new Ingredient('Flour', 200)]),
+    new Recipe('Recipe 2', 'This is a test',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkbElx8kYynTvJxzV0HkoXgPHqAJ96aal_vHRuLT4tanPbUqnq'
+      , [new Ingredient('Apple', 1)])
+  ];
 
   getRecipes() {
     // defensive copy through slice() because without it, a real reference to the array would be returned
